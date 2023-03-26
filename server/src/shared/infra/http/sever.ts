0e9024c0ae01e7aa01f9069e7fae0f351e472daa
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-03-26 18:44:25 -0300

    feat: :art: start server

---
+++ server/src/shared/infra/http/sever.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const SERVER_PORT = 3333
+
+const server = express()
+
+server.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  })
+})
+
+server.listen(SERVER_PORT, () => {
+  console.log(`===> Server started on port ${SERVER_PORT}`)
+})
