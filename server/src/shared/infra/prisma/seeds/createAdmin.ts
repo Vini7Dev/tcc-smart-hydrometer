65f56db3d298d15401be1399105000c3aa637aff
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-03-26 20:50:18 -0300

    feat: :art: create admin seed

---
+++ server/src/shared/infra/prisma/seeds/createAdmin.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { BCryptHashProvider } from '@shared/containers/providers/HashProvider/implementations/BCryptHashProvider'
+
+const prisma = new PrismaClient()
+
+const SEED_ADMIN_NAME = process.env.SEED_ADMIN_NAME
+const SEED_ADMIN_EMAIL = process.env.SEED_ADMIN_EMAIL
+const SEED_ADMIN_PASSWORD = process.env.SEED_ADMIN_PASSWORD
+
+export const createAdmin = async () => {
+  console.log('[SEED] Creating seed admin...')
+
+  if (!SEED_ADMIN_NAME || !SEED_ADMIN_EMAIL || !SEED_ADMIN_PASSWORD) {
+    throw new Error('Please set admin seed data.')
+  }
+
+  const seedAdminEmailAlreadyExists = await prisma.users.findFirst({
+    where: { email: SEED_ADMIN_EMAIL }
+  })
+
+  if (seedAdminEmailAlreadyExists) {
+    throw new Error('This seed admin email already exists, please use another email.')
+  }
+
+  const bCryptHashProvider = new BCryptHashProvider()
+
+  const passwordHash = await bCryptHashProvider.generateHash(SEED_ADMIN_PASSWORD)
+
+  await prisma.users.create({
+    data: {
+      name: SEED_ADMIN_NAME,
+      email: SEED_ADMIN_EMAIL,
+      password: passwordHash,
+    }
+  })
+
+  console.log('[SEED] Seed admin created!')
+}
+
+createAdmin()
+  .then(() => console.log('[FINISH] Seed executed successfully!'))
+  .catch((err) => console.error(`[ERROR] ${err.message}`))
